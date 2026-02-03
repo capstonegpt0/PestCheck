1a4d67bdea90b1f7abeaa486811bbedc5623a26c
capstonegpt <capstonegpt0@gmail.com>
2026-02-04 01:15:28 +0800

    remove expert role

--- src/components/HeatMap.jsx
+++ src/components/HeatMap.jsx
@@ -487,6 +487,15 @@ const HeatMap = ({ user, onLogout }) => {
             </select>
           </div>
 
+          <button
+            onClick={fetchInitialData}
+            className="flex items-center px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
+            title="Refresh map data"
+          >
+            <Activity className="w-5 h-5 mr-2" />
+            Refresh
+          </button>
+
           <div className="flex-1"></div>
 
           <button
