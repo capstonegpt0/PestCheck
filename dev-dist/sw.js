b239a8e1b1781debadecfb2f0314c965df1585a2
capstonegpt <capstonegpt0@gmail.com>
2026-04-13 10:38:05 +0800

    template fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.15mf8mpuvd"
+    "revision": "0.0mlsbdd97lg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
